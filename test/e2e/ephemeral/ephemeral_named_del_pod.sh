13cab9942b1e88045993b2656bb5afd393724507
Zbynek Roubalik <zroubali@redhat.com>
2017-08-31 16:32:45 +0200

    fix

--- test/e2e/ephemeral/ephemeral_named_del_pod.sh
+++ test/e2e/ephemeral/ephemeral_named_del_pod.sh
@@ -6,13 +6,14 @@ SCRIPT_DIR=$(readlink -f `dirname "${BASH_SOURCE[0]}"`)
 source $TEST_DIR/common
 source $SCRIPT_DIR/del_pod
 
-set_worker_count $S2I_TEST_WORKERS
+#set_worker_count $S2I_TEST_WORKERS
 
 os::test::junit::declare_suite_start "$MY_SCRIPT"
 
 # Make the S2I test image if it's not already in the project
-make_image
+#make_image
 
-del_pod_tests true
+#del_pod_tests true
+return 1
 
 os::test::junit::declare_suite_end
